cb04e8df2f95b10cb5d92e80b436bb0db41ce097
will-johnston-goodyear <will_johnston@goodyear.com>
2022-11-02 14:44:48 -0400

    Adds embeded image

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,35 @@
 import * as React from "react";
 import type { HeadFC } from "gatsby";
 import styled from "styled-components";
+//@ts-ignore
+import elevatorPitch from "../images/NimbusElevatorPitch.png";
 
-const StyledSpan = styled.span`
-  font-family: "Titan One", cursive;
+const ImageWrapper = styled.div`
+  width: 100%;
+  height: 100%;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  background-color: #f1e3e4;
+  background: linear-gradient(180deg, #f1e3e4 0%, #f1e3e4 50%, #bb9bb0 100%);
+`;
+
+const ElevatorPitchImage = styled.img`
+  /* width: clamp(50%, 75%, 100%); */
+  width: 75%;
+
+  @media (max-width: 500px) {
+    width: 100%;
+  }
 `;
 const IndexPage = () => {
-  return <StyledSpan>Nimbus: Cloud Enabled Point-of-Sale</StyledSpan>;
+  return (
+    <ImageWrapper>
+      <ElevatorPitchImage src={elevatorPitch} />
+    </ImageWrapper>
+  );
 };
 
 export default IndexPage;
